e45fb88cdf531f6dd873e494c04c2013c3454eaf
Ankit <ankit@kashware.com>
2023-02-22 11:40:30 +0530

    added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/ask/")
 async def ask_question(question: str):
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     # create a completion
     completion = openai.Completion.create(
